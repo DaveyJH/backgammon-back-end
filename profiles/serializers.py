e56176f44832160f7db160a9dcdc1fff4e62f6df
DaveyJH <daveyj.dh@googlemail.com>
2024-07-05 22:54:27 +0100

    feat: Add game and win counts to Profile views

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,10 +1,45 @@
 from rest_framework import serializers
 from .models import Profile
+from games.models import Game
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
+    active_games_count = serializers.SerializerMethodField()
+    finished_games_count = serializers.SerializerMethodField()
+    total_games_count = serializers.SerializerMethodField()
+    total_wins_count = serializers.SerializerMethodField()
+
+    def get_active_games_count(self, obj):
+        return Game.objects.filter(
+            active=True,
+            player1=obj.owner,
+        ).count() + Game.objects.filter(
+            active=True,
+            player2=obj.owner,
+        ).count()
+
+    def get_finished_games_count(self, obj):
+        return Game.objects.filter(
+            active=False,
+            player1=obj.owner,
+        ).count() + Game.objects.filter(
+            active=False,
+            player2=obj.owner,
+        ).count()
+
+    def get_total_games_count(self, obj):
+        return Game.objects.filter(
+            player1=obj.owner,
+        ).count() + Game.objects.filter(
+            player2=obj.owner,
+        ).count()
+
+    def get_total_wins_count(self, obj):
+        return Game.objects.all().filter(
+            winner__owner=obj.owner,
+        ).count()
 
     def get_is_owner(self, obj):
         request = self.context["request"]
@@ -18,4 +53,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             "info",
             "image",
             "is_owner",
+            "active_games_count",
+            "finished_games_count",
+            "total_games_count",
+            "total_wins_count",
         ]
