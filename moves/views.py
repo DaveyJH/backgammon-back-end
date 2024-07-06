2e88d5e2d52a7397de49fdd805a6cc145fce9db2
DaveyJH <daveyj.dh@googlemail.com>
2024-07-06 12:25:51 +0100

    refactor: Remove unnecessary retrieval of Game object

--- moves/views.py
+++ moves/views.py
@@ -4,18 +4,16 @@ from rest_framework.exceptions import PermissionDenied
 from django_filters.rest_framework import DjangoFilterBackend
 from backgammon_drf.permissions import (
     IsOwnerOrReadOnly,
-    IsPlayerOrReadOnly,
     IsMostRecentMove
 )
 from .models import Move
 from .serializers import MoveSerializer
 from .filters import MoveFilter
-from games.models import Game
 
 
 class MoveList(generics.ListCreateAPIView):
     serializer_class = MoveSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsPlayerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Move.objects.all().order_by("-updated_at")
     filter_backends = [
         filters.OrderingFilter,
@@ -32,10 +30,9 @@ class MoveList(generics.ListCreateAPIView):
     ]
 
     def perform_create(self, serializer):
-        game_id = serializer.validated_data['game'].id
-        game = Game.objects.get(id=game_id)
+        game_obj = serializer.validated_data['game']
 
-        if self.request.user not in [game.player1, game.player2]:
+        if self.request.user not in [game_obj.player1, game_obj.player2]:
             raise PermissionDenied("You are not a player in this game.")
 
         serializer.save(owner=self.request.user)
@@ -44,7 +41,7 @@ class MoveList(generics.ListCreateAPIView):
 class MoveDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MoveSerializer
     permission_classes = [IsOwnerOrReadOnly, IsMostRecentMove]
-    queryset = Move.objects.all().order_by("-updated_at")
+    queryset = Move.objects.all()
 
     def perform_update(self, serializer):
         if self.get_object().game.id != int(self.request.data["game"]):
