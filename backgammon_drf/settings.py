f3e4514e46ed63046c395dd67a366b099bd05534
DaveyJH <daveyj.dh@googlemail.com>
2024-07-06 14:35:32 +0100

    fix: User serializer cloned from ci drf walkthrough

--- backgammon_drf/settings.py
+++ backgammon_drf/settings.py
@@ -55,7 +55,8 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER':
+        'backgammon_drf.serializers.CurrentUserSerializer'
 }
 
 # Application definition
